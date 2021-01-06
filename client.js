e3579cfd33fd4e0f6da683aabaf4bedf9b127a86
Anh Le <a.v.le@wustl.edu>
2021-01-06 17:32:53 +0700

    Continue working on logout functionality

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#login_btn").click(function () {
-        console.log("log in button pressed");
+        console.log("log in pressed");
         let username = $("#username").val().trim();
         let password = $("#password").val().trim();
         $.ajax({
@@ -14,18 +14,20 @@ $(document).ready(function () {
             success: function(response){
                 console.log(response);
                 var msg = "";
-                if(response == 1){
+                if(parseInt(response.status) <= 299){
                     msg = "Login Succesfully";
+                    $("#logout_btn").show();
+                    $('#login_btn').hide();
                 }
                 else{
-                    msg = "Invalid username or password!";
+                    msg = "Incorrect username or password!";
                 }
-                $("#message").html(response);
+                $("#login_message").html(msg);
             }
         });
     });
     $("#register_btn").click(function () {
-        console.log("register button pressed");
+        console.log("register pressed");
         let username = $("#register_username").val().trim();
         let password = $("#register_password").val().trim();
         let email = $("#register_email").val().trim();
@@ -41,13 +43,39 @@ $(document).ready(function () {
             success: function(response){
                 console.log(response);
                 var msg = "";
-                if(response == 1){
-                    msg = "Regiser Succesfully";
+                if(parseInt(response.status) <= 299){
+                    msg = "Register Succesfully";
                 }
                 else{
-                    msg = "Regiser Unsuccesfully";
+                    msg = "Register Unsuccesfully";
                 }
-                $("#message").html(response);
+                $("#register_message").html(msg);
+            }
+        });
+    });
+
+    $("#logout_btn").click(function () {
+        console.log("logout pressed");
+        $.ajax({
+            url:'http://localhost:3000/user/logout',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            type:"GET",
+            dataType: 'json',
+            // data: { username:username, password:password, email: email, fullname: fullname },
+            success: function(response){
+                console.log(response);
+                var msg = "";
+                // if(parseInt(response.status) <= 299){
+                //     msg = "Logout Succesfully";
+                //     $("#logout_btn").hide();
+                //     $('#login_btn').show();
+                // }
+                // else{
+                //     msg = "Logout Unsuccesfully";
+                // }
+                // $("#login_message").html(msg);
             }
         });
     });
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const moment = require('moment');
 var cors = require('cors');
 const saltRounds = 10;
+require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
@@ -33,16 +34,15 @@ app.get("/", (req, res) => {
             res.send(error);
         }
         if (user.length > 0) {
-            res.json(user)
+            res.json(user);
         }         
     });
 });
 
+//---------------------------- Login ----------------------------
 app.post("/user/login", (req, res) => {
-    // console.log(req.body);
     let username = req.body.username;
     let password = req.body.password;
-    // res.json({ name: "anh le"})
     if (username && password) {
         db.query('SELECT * FROM user WHERE username = ?', [username], function(error, user) {
             if(error){ 
@@ -52,18 +52,18 @@ app.post("/user/login", (req, res) => {
                 bcrypt.compare(password, user[0].password, function(err, result) {
                     if(result){
                         db.query("UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE username = ?", [username]);
-                        // const token = jwt.sign({ username }, process.env.JWT_SECRET_TOKEN, {
-                        //     expiresIn: process.env.JWT_EXPIRE_IN
-                        // });
+                        const token = jwt.sign({ username }, process.env.JWT_SECRET_TOKEN, {
+                            expiresIn: process.env.JWT_EXPIRE_IN
+                        });
                         
-                        // const cookieOptions = {
-                        //     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-                        //     httpOnly: true
-                        // }
+                        const cookieOptions = {
+                            expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                            httpOnly: true
+                        }
 
-                        // res.cookie('jwt', token, cookieOptions);
+                        res.cookie('jwt', token, cookieOptions);
                         // res.status(200).redirect('/');
-                        res.json({ status : 200, message: "User logged in", user : user[0]});
+                        res.json({ status : 200, token: token, message: "User logged in", user : user[0]});
                     }
                     else{
                         res.json({ status : 404, message: "Incorrect Password"});
@@ -80,12 +80,13 @@ app.post("/user/login", (req, res) => {
     }
 });
 
+//--------------------------- Register ---------------------------
 app.post("/user/register", (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
     let fullname = req.body.fullname;
     let email = req.body.email;
-    db.query('SELECT * FROM user WHERE username = ?', [username], function(error, results, fields) {
+    db.query('SELECT * FROM user WHERE username = ?', [username], function(error, results) {
         if(error) throw error;
         if (results.length > 0) {
             res.json({ status : 404, message: "Duplicate User"});
@@ -103,6 +104,20 @@ app.post("/user/register", (req, res) => {
         }
     });
 });
+
+//---------------------------- Logout ----------------------------
+app.get('/user/logout', (req, res) => {
+    // req.session.destroy(function(err){  
+    //     if(err){  
+    //         console.log(err);  
+    //     }  
+    //     else{  
+    //         res.redirect('/');  
+    //     }  
+    // });
+    delete req.session; 
+    res.send("Hello");
+})
 
 //----------------------------- Port -----------------------------
 app.listen(3000, () => {
